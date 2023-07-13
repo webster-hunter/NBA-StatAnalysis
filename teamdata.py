1f4f1443ca8fe9a609bfdd4ce5b491e2cac310c8
Hunter <whwebster98@live.com>
2023-07-13 16:03:16 -0500

    removed redundant files

--- teamdata.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-import pandas as pd
-
-def getTeamYear(conn, team, year):
-    query = "SELECT * FROM player_box_score WHERE Team=\""+team+"\" AND Season="+year
-    df = pd.read_sql_query(query, conn)
-    print(df.to_markdown())
